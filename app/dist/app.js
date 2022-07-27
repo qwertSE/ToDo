bbcfccbe94190e6e77a8c4b28fa25241eebd9287
Dan <danilobomfim.si@gmail.com>
2022-07-27 20:19:33 -0300

    feat

--- app/dist/app.js
+++ app/dist/app.js
@@ -1,47 +1,72 @@
+class Task {
+    constructor(description, done) {
+        this.description = description;
+        this.done = done;
+    }
+    get descri() {
+        return this.description;
+    }
+    set descri(value) {
+        this.description = value;
+    }
+    get isDone() {
+        return this.done;
+    }
+    set isDone(value) {
+        this.done = value;
+    }
+}
 var listElements = [];
 const inputText = document.getElementById("input_text");
 var db = localStorage;
 var index;
 function add() {
+    let temp = new Task(inputText.value, false);
     index = String(listElements.length);
-    createElement(inputText.value);
-    db.setItem(index, inputText.value);
+    createElement(temp);
+    db.setItem(index, JSON.stringify(listElements[Number(index)]));
 }
 function removeItem(id) {
-    var element = document.getElementById(`div${id}`);
+    let element = document.getElementById(`div${id}`);
+    console.log(id);
+    db.removeItem(String(id));
+    listElements.splice(id, 1);
     db.clear();
-    listElements.splice(listElements.indexOf(element.innerText), 1);
-    for (let i = 0; i < listElements.length; i++) {
-        index = String(i);
-        db.setItem(index, listElements[i]);
-    }
+    listElements.forEach(function (element, index) {
+        db.setItem(String(index), JSON.stringify(element));
+    });
+    loadDb();
     location.reload();
 }
 function updateItem(id) {
     document.getElementById(id).innerText = inputText.value;
-    listElements[parseInt(id)] = inputText.value;
-    listElements[Number(id)] = inputText.value;
-    db.setItem(id, inputText.value);
+    listElements[Number(id)].descri = inputText.value;
+    db.setItem(id, JSON.stringify(listElements[Number(id)]));
     loadDb();
     location.reload();
 }
 function check(id) {
     const texto = document.getElementById(`${id}`);
-    if (texto.classList.contains("done")) {
+    if (listElements[id].isDone) {
+        listElements[id].isDone = false;
         texto.classList.remove("done");
+        db.setItem(String(id), JSON.stringify(listElements[Number(id)]));
     }
     else {
+        listElements[id].isDone = true;
+        db.setItem(String(id), JSON.stringify(listElements[Number(id)]));
         texto.classList.add("done");
     }
 }
-function createElement(value) {
+function createElement(obj) {
+    let temp = obj;
     let item = `<div class="todo__item" id="div${listElements.length}">
   <button class="check" onclick="check(${listElements.length})"></button>
-  <h2 class="text" id="${listElements.length}">${value}</h2>
+  <h2 class="text" id="${listElements.length}">${temp.descri}</h2>
   <button class="del" onclick="removeItem(${listElements.length})"></button>
   <button class="update" onclick="updateItem(${listElements.length})"></button>
 </div>`;
-    listElements.push(value);
+    listElements.push(obj);
     let ul = document.getElementById("container-item");
     let li = document.createElement("li");
     li.innerHTML = item;
@@ -50,10 +75,15 @@ function createElement(value) {
 function loadDb() {
     let dbSize = db.length;
     let temp;
-    if (listElements.length === 0) {
+    if (dbSize !== 0) {
         for (let i = 0; i < dbSize; i++) {
-            temp = db.getItem(String(i));
-            createElement(temp);
+            temp = JSON.parse(db.getItem(String(i)));
+            let task = new Task(temp["description"], temp["done"]);
+            createElement(task);
+            if (task.isDone) {
+                const texto = document.getElementById(`${i}`);
+                texto.classList.add("done");
+            }
         }
     }
 }
--- app/src/app.ts
+++ app/src/app.ts
@@ -1,4 +1,33 @@
-var listElements: string[] = [];
+
+class Task {
+  private description: string;
+  private done: boolean;
+
+  constructor(description: string, done: boolean) {
+    this.description = description;
+    this.done = done;
+  }
+
+  public get descri(): string {
+    return this.description;
+  }
+
+  public set descri(value: string) {
+    this.description = value;
+  }
+
+  public get isDone(): boolean {
+    return this.done;
+  }
+
+  public set isDone(value: boolean) {
+    this.done = value;
+  }
+}
+
+
+/* Variaveis*/
+var listElements: Task[] = [];
 const inputText = document.getElementById("input_text") as HTMLInputElement;
 var db = localStorage;
 var index: string;
@@ -15,57 +44,69 @@ var index: string;
 
 /* Criando um elemento pela primeira vez usando o layout. */
 function add(): void {
+  let temp = new Task(inputText.value, false);
   index = String(listElements.length);
-  createElement(inputText.value);
 
-  db.setItem(index, inputText.value);
+  createElement(temp);
+
+  db.setItem(index, JSON.stringify(listElements[Number(index)]));
 }
 
+
+/* Remove o item do array e do localStorage fazendo um reload na pagina para organizalos. */
 function removeItem(id: number): void {
-  var element = document.getElementById(`div${id}`);
+  let element = document.getElementById(`div${id}`);
+  console.log(id);
 
+  db.removeItem(String(id));
+  listElements.splice(id, 1);
   db.clear();
-  listElements.splice(listElements.indexOf(element.innerText), 1);
-  for (let i = 0; i < listElements.length; i++) {
-    index = String(i);
-    db.setItem(index, listElements[i]);
-  }
-  location.reload();
 
-  /* console.log(listElements) */
-}
+  listElements.forEach(function (element, index) {
+    db.setItem(String(index), JSON.stringify(element));
+  });
 
-function updateItem(id: string): void {
-  document.getElementById(id).innerText = inputText.value;
-  listElements[parseInt(id)] = inputText.value;
-
-  listElements[Number(id)] = inputText.value;
-  db.setItem(id, inputText.value);
   loadDb();
   location.reload();
 }
 
+/* Atualiza o valor da task selecionada. */
+function updateItem(id: string): void {
+  document.getElementById(id).innerText = inputText.value;
+
+  listElements[Number(id)].descri = inputText.value;
+  db.setItem(id, JSON.stringify(listElements[Number(id)]));
+  loadDb();
+  location.reload();
+}
+
+/* Marca como completa a task selecionada. */
 function check(id: number): void {
   const texto = document.getElementById(`${id}`);
 
-  /* console.log(texto.classList); */
-
-  if (texto.classList.contains("done")) {
+  if (listElements[id].isDone) {
+    listElements[id].isDone = false;
     texto.classList.remove("done");
+    db.setItem(String(id), JSON.stringify(listElements[Number(id)]));
   } else {
+    listElements[id].isDone = true;
+    db.setItem(String(id), JSON.stringify(listElements[Number(id)]));
     texto.classList.add("done");
   }
 }
 
-function createElement(value: string): void {
+/* Função de criação do elemento HTML */
+function createElement(obj: Task): void {
+  let temp = obj;
+
   let item = `<div class="todo__item" id="div${listElements.length}">
   <button class="check" onclick="check(${listElements.length})"></button>
-  <h2 class="text" id="${listElements.length}">${value}</h2>
+  <h2 class="text" id="${listElements.length}">${temp.descri}</h2>
   <button class="del" onclick="removeItem(${listElements.length})"></button>
   <button class="update" onclick="updateItem(${listElements.length})"></button>
 </div>`;
 
-  listElements.push(value);
+  listElements.push(obj);
   let ul = document.getElementById("container-item");
   let li = document.createElement("li");
 
@@ -73,13 +114,23 @@ function createElement(value: string): void {
   ul.appendChild(li);
 }
 
+
+/* Função para carregamento do LocalStorage caso exista. */
 function loadDb() {
   let dbSize = db.length;
-  let temp: string;
-  if (listElements.length === 0) {
+  let temp;
+
+  if (dbSize !== 0) {
     for (let i = 0; i < dbSize; i++) {
-      temp = db.getItem(String(i));
-      createElement(temp);
+      temp = JSON.parse(db.getItem(String(i)));
+      let task = new Task(temp["description"], temp["done"]);
+      createElement(task);
+
+      /* Verifica se a task estava marcada como completa e modifica o elemento HTML */
+      if (task.isDone) {
+        const texto = document.getElementById(`${i}`);
+        texto.classList.add("done");
+      }
     }
   }
 }
